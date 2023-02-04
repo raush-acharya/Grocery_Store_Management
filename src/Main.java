0bd3aaf7381b787127d9cde37623b33119a8449c
Raush <68884436+raush-acharya@users.noreply.github.com>
2023-02-04 09:50:34 +0545

    switch case

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int continue_input;
         do {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
             System.out.println("Please type the corresponding number to select one of the following options." );
             System.out.println("1: Add items to inventory");
             System.out.println("2: View inventory");
             System.out.println("3: Sales Page");
             System.out.println("4: Exit");
+
             continue_input = scanner.nextInt();
+
+            switch (continue_input){
+                case 1:
+//                    add items to inventory
+                    break;
+                case 2:
+//                    view inventory
+                    break;
+                case 3:
+//                    Sales page
+                case 4:
+                    System.out.println("Thank you for using Grocery Store Management System");
+            }
         }while (continue_input != 4);
     }
 }
