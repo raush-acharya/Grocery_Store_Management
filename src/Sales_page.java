3b9d13eae16df84410638142bdd6e9bdf93b98d2
Raush <68884436+raush-acharya@users.noreply.github.com>
2023-02-04 13:24:50 +0545

    sales page

--- src/Sales_page.java
+++ src/Sales_page.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Sales_page {
     public static void Sales_page() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Sales Page");
         System.out.println("Here is the inventory:");
+        System.out.println("Press any key to continue");
+        scanner.nextLine();
     }
 }
