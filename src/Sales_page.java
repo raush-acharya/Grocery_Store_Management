71c14b241b9858d791efe03f5177602467c0bf2a
Raush <68884436+raush-acharya@users.noreply.github.com>
2023-02-06 19:06:11 +0545

    Sales page complete in my opinion Bug testing left

--- src/Sales_page.java
+++ src/Sales_page.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sales_page {
     static int S_N;
     static String item;
     static int quantity;
-    public static void Sales_page() {
+    static ArrayList<String> bill_items = new ArrayList<String>();
+    static ArrayList<Integer> bill_quantity = new ArrayList<Integer>();
 
+    static Scanner scanner = new Scanner(System.in);
+    public static void Sales_page() {
+        int index;
         int input_listener;
         boolean loop_runner;
 
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to Sales Page");
         Inventory.Inventory();
         do{
@@ -17,8 +22,29 @@ public class Sales_page {
             S_N = scanner.nextInt();
             scanner.nextLine();
             item = Add_items.Send_items(S_N-1);
-            System.out.println("How many "+ item +"s do you want to buy?");
-            quantity =  scanner.nextInt();
+
+//            To check if user has already bought the item
+            if (bill_items.contains(item)){
+                index = bill_items.indexOf(item);
+                System.out.println("You've already bought this item.");
+                System.out.println("Enter How many "+ bill_items.get(index) + "s you want to add");
+
+//                To check whether we have enough quantity available
+                int quantity_check = scanner.nextInt() + bill_quantity.get(index);
+                quantity = Sales_page.Quantity_checker(quantity_check,index);
+
+
+            }
+            else {
+                index = S_N-1;
+                bill_items.add(item);
+                System.out.println("How many "+ item +"s do you want to buy?");
+                int quantity_check = scanner.nextInt();
+                quantity =  Sales_page.Quantity_checker(quantity_check,index);
+                scanner.nextLine();
+
+                bill_quantity.add(quantity);
+            }
             Add_items.change_quantity();
             System.out.println("Press 1 to continue shopping.");
             System.out.println("Press 2 to generate bill.");
@@ -26,10 +52,13 @@ public class Sales_page {
             scanner.nextLine();
 
             switch (input_listener){
-                case 1: loop_runner = true;
-                break;
-                case 2: loop_runner = false;
-                break;
+                case 1:
+                    loop_runner = true;
+                    break;
+                case 2:
+                    loop_runner = false;
+                    generate_bill();
+                    break;
                 default:
                     System.out.println("Please enter 1 to continue shopping or 2 to generate bill.");
                     loop_runner = true;
@@ -38,10 +67,44 @@ public class Sales_page {
         }while (loop_runner);
     }
 
+    public static void generate_bill(){
+        double Grand_total = 0;
+        System.out.println("Here is your bill");
+        System.out.println("Sn.    Items       Price     Quantity      Total");
+
+        for(int counter = 0; counter < bill_items.size(); counter++){
+            String Item = bill_items.get(counter);
+            int Quantity = bill_quantity.get(counter);
+            double Price = Add_items.Send_price(counter);
+            double total = Quantity * Price;
+            Grand_total = Grand_total + total;
+            System.out.println(counter+1 + "      " + Item + "      " + Price + "      " + Quantity + "      " + total);
+        }
+        System.out.println("Please pay Rs."+ Grand_total);
+        System.out.println("Thank you for shopping using Grocery Store Management System");
+        System.out.println("Press any key to continue.");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        bill_quantity.clear();
+        bill_items.clear();
+    }
     public static int user_selection() {
         return S_N-1;
     }
     public static int send_quantity() {
         return quantity;
     }
+
+    public static int Quantity_checker(int quantity_check, int index){
+
+        if (quantity_check > Add_items.Send_quantity(index)){
+            System.out.println("Sorry we don't have enough "+ item + "s.");
+            return 0;
+        }
+        else {
+            return quantity_check;
+        }
+
+    }
 }
+
